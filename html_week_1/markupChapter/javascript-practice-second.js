886a9e82bcd55e9fdb3ce6e26aabe7c6aed8f59a
mikeyoutz <ztuoy@comcast.net>
2015-04-16 19:38:29 -0600

    initial add

---
+++ html_week_1/markupChapter/javascript-practice-second.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Mike_HP-Lap on 3/26/2015.
+ */
+
+var mike = 750;
+var newNumber = 200;
+var thirdNumber = "newNumber + mike";
+console.log(mike);
+
+var mike = 125;
+
+console.log(mike);
---
+++ html_week_1/markupChapter/javascript-practice_3-24-15.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Mike_HP-Lap on 3/26/2015.
+ */
+
+var dave ={name: "value"}
+
+console.log(name);
